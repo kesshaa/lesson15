72102d84dee5c218f69527eabb74a641dcece7c7
kesshaa <133736934+kesshaa@users.noreply.github.com>
2024-12-06 11:54:52 +0300

    Add files via upload

---
+++ lesson15/zad6/src/Main.java
@@ -0,0 +1,50 @@
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Comparator;
+
+public class Main {
+    private static Set<Letter> letters = new LinkedHashSet<>();
+
+    public static void main(String[] args) {
+        // информация о письмах (в порядке занесения в систему)
+        letters.add(new Letter("Джон Смит", LocalDate.of(2021, 7, 7), "текст письма №1 ..."));
+        letters.add(new Letter("Аманда Линс", LocalDate.of(2021, 6, 17), "текст письма №2 ..."));
+        letters.add(new Letter("Джо Кью", LocalDate.of(2021, 7, 5), "текст письма №3 ..."));
+        letters.add(new Letter("Мишель Фернандес", LocalDate.of(2021, 8, 23), "текст письма №4 ..."));
+
+        printOrderedById(letters);
+        printOrderedByDateReceived(letters);
+    }
+
+    private static void printOrderedById(Set<Letter> letters) {
+        System.out.println("Все письма с сортировкой по ID: ");
+
+        for (Letter letter : letters) {
+            System.out.println("    * Письмо от " + letter.authorName + " поступило " + letter.dateReceived);
+        }
+    }
+
+    private static void printOrderedByDateReceived(Set<Letter> letters) {
+        System.out.println("Все письма с сортировкой по дате получения: ");
+
+        Set<Letter> sortedletter = new TreeSet<>(Comparator.comparing(l -> l.dateReceived));
+        sortedletter.addAll(letters);
+        for (Letter letter : sortedletter) {
+            System.out.println("    * Письмо от " + letter.authorName + " поступило " + letter.dateReceived);
+        }
+    }
+
+    static class Letter {
+        public String authorName;      // имя отправителя
+        public LocalDate dateReceived; // дата получения письма
+        public String text;            // текст письма
+
+        public Letter(String senderName, LocalDate dateReceived, String text) {
+            this.authorName = senderName;
+            this.dateReceived = dateReceived;
+            this.text = text;
+        }
+    }
+}
